37a734ad73e519952780e1c324404d3f0bb478d8
mmccabe05 <mmccabe@chicagobooth.edu>
2023-01-13 04:37:22 +0000

    added enums and biz logic

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -26,5 +26,8 @@ class Photo < ApplicationRecord
   has_many :fans, through: :likes
   validates :caption, presence: true
   validates :image, presence: true
-  
+
+scope :past_week, -> { where(created_at: 1.week.ago...) }
+
+scope :by_likes, -> { order(likes_count: :desc) }
 end
