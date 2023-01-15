6de8c404899e4648aca099d680ffacb6e3cb2edf
mmccabe05 <mmccabe@chicagobooth.edu>
2023-01-15 04:03:23 +0000

    updated user menu paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
   root "photos#index"
 
-
   devise_for :users
 
   resources :comments
   resources :follow_requests
   resources :likes
   resources :photos
-  resources :users, only: :show  # get "/users/:id" => "users#show", as: :user
+  # resources :users, only: :show  # get "/users/:id" => "users#show", as: :user
+  get ":username/liked" => "photos#liked", as: :liked_photos
 
-# must stay at the end, very general much problems if higher
-  get "/:username" => "users#show"
+  get ":username" => "users#show", as: :user
+  # must stay at the end, very general much problems if higher
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
